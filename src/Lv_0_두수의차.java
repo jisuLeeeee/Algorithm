9fc6b61894a410b6428015ef108c5349bc15e35c
jisuLeeeee <jisu.lee2001@gmail.com>
2022-12-09 23:50:09 +0900

    edit commit

--- src/Lv_0_두수의차.java
+++ src/Lv_0_두수의차.java
@@ -1,12 +1,7 @@
 public class Lv_0_두수의차 {
-    class Solution {
-        public int solution(int num1, int num2) {
-            int answer = 0;
-            answer = num1 - num2;
-            return answer;
-        }
-    }
-    public static void main(String[] args) {
-
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        answer = num1 - num2;
+        return answer;
     }
 }
