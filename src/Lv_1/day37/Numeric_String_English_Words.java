681d7d4c6e238d4f8a0468338260a595b6c95621
jisuLeeeee <jisu.lee2001@gmail.com>
2023-05-04 13:45:25 +0900

    38일차 문제_숫자 문자열과 영단어

---
+++ src/Lv_1/day37/Numeric_String_English_Words.java
@@ -0,0 +1,24 @@
+package Lv_1.day37;
+
+public class Numeric_String_English_Words {
+    public int solution(String s) {
+        int answer = 0;
+
+        String[] eng = {"zero", "one", "two", "three", "four", "five", "six",
+                "seven", "eight", "nine"};
+
+        for(int i=0; i<eng.length; i++){
+            if(s.contains(eng[i])){
+                s = s.replace(eng[i], String.valueOf(i));
+            }
+        }
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Numeric_String_English_Words numericStringEnglishWords = new Numeric_String_English_Words();
+        String a = "one4seveneight";
+        System.out.println(numericStringEnglishWords.solution(a));
+    }
+}
