6a7fef7115852350cc0ec4905f5401a2ec7994d8
jisuLeeeee <jisu.lee2001@gmail.com>
2023-05-18 16:06:06 +0900

    45일차 문제_점프와순간이동 설명수정

--- src/Lv_2/day45/Jump_Teleportation.java
+++ src/Lv_2/day45/Jump_Teleportation.java
@@ -3,7 +3,6 @@ package Lv_2.day45;
 public class Jump_Teleportation {
     public int solution(int n) {
         int ans = 0;
-        int distance = 0;
 
         while(n != 0){
             if(n % 2 == 0)
