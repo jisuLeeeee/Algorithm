902e8ac3252f04b0bc7496c514ead4739e87e736
jisuLeeeee <jisu.lee2001@gmail.com>
2023-05-18 16:05:22 +0900

    45일차 문제_점프와순간이동

---
+++ src/Lv_2/day45/Jump_Teleportation.java
@@ -0,0 +1,25 @@
+package Lv_2.day45;
+
+public class Jump_Teleportation {
+    public int solution(int n) {
+        int ans = 0;
+        int distance = 0;
+
+        while(n != 0){
+            if(n % 2 == 0)
+                n /= 2;
+            else {
+                n--;
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Jump_Teleportation jumpTeleportation = new Jump_Teleportation();
+        int a = 5;
+        System.out.println(jumpTeleportation.solution(a));
+    }
+}
